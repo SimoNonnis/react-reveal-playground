571740cf7e9cc969c835892487034e62bdd837d8
Simon Nonnis <SimoNonnis@users.noreply.github.com>
2020-04-24 10:45:07 +0100

    Add react-router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
-import React from "react";
-import "./styles.css";
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+
+import Home from './components/Home';
+
+import './styles.css';
 
 export default function App() {
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
+    <Router>
+      <div className="App">
+        <h1>React Reveal Playground</h1>
+
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+        </ul>
+      </div>
+
+      <Switch>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
