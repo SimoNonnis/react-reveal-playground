2bf9251014ed76aee451d2824151797e68346f15
Simon Nonnis <SimoNonnis@users.noreply.github.com>
2020-04-26 10:23:19 +0100

    Add Roll

--- src/components/Roll.jsx
+++ src/components/Roll.jsx
@@ -1,5 +1,10 @@
 import React from 'react';
+import Roll from 'react-reveal/Roll';
 
-const RollPage = () => <h2>Roll</h2>;
+const RollPage = () => (
+  <Roll>
+    <h2>Roll</h2>
+  </Roll>
+);
 
 export default RollPage;
